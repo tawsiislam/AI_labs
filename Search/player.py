7ffe67e99f6196355d46d5a08eaaee89bf8886e8
ashenawa <ashenawa@kth.se>
2023-09-11 11:11:37 +0200

    analysing code, found param for heuristic function

--- Search/player.py
+++ Search/player.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import random
 
-from fishing_game_core.game_tree import Node
+from fishing_game_core.game_tree import Node, State
 from fishing_game_core.player_utils import PlayerController
 from fishing_game_core.shared import ACTION_TO_STR
-
+from typing import List, Tuple # for type hinting
+import numpy as np
 
 class PlayerControllerHuman(PlayerController):
     def player_loop(self):
@@ -28,6 +29,7 @@ class PlayerControllerMinimax(PlayerController):
 
     def __init__(self):
         super(PlayerControllerMinimax, self).__init__()
+        
 
     def player_loop(self):
         """
@@ -41,8 +43,34 @@ class PlayerControllerMinimax(PlayerController):
         while True:
             msg = self.receiver()
 
+            
+            """ 
+            message is a dictionary with the following keys:
+            dict_keys(['game_over', 'hooks_positions', 'fishes_positions', 'observations', 'fish_scores', 'player_scores', 'caught_fish'])
+
+            'game_over' is a boolean indicating if the game is over
+            
+            'hooks_positions' is a dictionary with keys 0 and 1, each containing a tuple with the position of the hook of each player
+
+            'fishes_positions' dictionary of tuples with the position of each fish
+
+            'observations' ?
+
+            'fish_scores'  same dictionary keys as 'fishes_positions' but with the score of each corresponding fish
+
+            'player_scores' is a dictionary with keys 0 and 1, each containing the score of each player
+
+            'caught_fish' is a dictionary with keys of corresponding fish that players are currently holding on hook, can check the fishes score value by using the key to index into 'fish_scores'
+
+            """
+
+
             # Create the root node of the game tree
             node = Node(message=msg, player=0)
+            
+            
+            
+
 
             # Possible next moves: "stay", "left", "right", "up", "down"
             best_move = self.search_best_next_move(initial_tree_node=node)
@@ -65,5 +93,59 @@ class PlayerControllerMinimax(PlayerController):
         # NOTE: Don't forget to initialize the children of the current node
         #       with its compute_and_get_children() method!
 
+        
+        
+
         random_move = random.randrange(5)
         return ACTION_TO_STR[random_move]
+
+
+
+
+
+# distance calculator, euclidian, takes 2 tuples and returns distance between them
+def EuclidianDist(coord1: Tuple, coord2:Tuple)->float:
+    """takes in two coordinates in form of tuples and returns distance between them"""
+
+    return np.sqrt( (coord2(1) -coord1(1))**2 + (coord2(2) -coord1(2))**2  )
+
+
+class ParentNode:
+    def __init__(self, currPoint:tuple, parent, heurVal:int = 0 ) -> None:
+        self.currentNode = currPoint
+        self.parent = parent
+        self.heuristicValue = heurVal
+        
+
+    def SetHeuristicValue(self, heusVal:int) ->None:
+        self.heuristicValue = heusVal
+        
+        
+def CheckIfTerminalState(state, depth, maxDepth, time, maxTime) -> bool:
+    """checks if the state is terminal or not, returns true if terminal, false if not"""
+    if depth == maxDepth or time == maxTime: # or state.game_over == True
+        return True
+    else:
+        return False
+
+
+# evaluation function
+#- heuristic function
+
+
+
+
+# minimax function
+#- max function
+#- min function
+
+
+# alpha beta pruning function
+
+
+
+
+
+
+
+
