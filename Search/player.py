eb7490a60fa8a5ba688c9eb4348eb42b24792212
ashenawa <ashenawa@kth.se>
2023-09-13 14:57:08 +0200

    started on heuristic, a/b pruning, depth search

--- Search/player.py
+++ Search/player.py
@@ -6,6 +6,7 @@ from fishing_game_core.player_utils import PlayerController
 from fishing_game_core.shared import ACTION_TO_STR
 from typing import List, Tuple # for type hinting
 import numpy as np
+import time
 
 class PlayerControllerHuman(PlayerController):
     def player_loop(self):
@@ -39,6 +40,10 @@ class PlayerControllerMinimax(PlayerController):
 
         # Generate first message (Do not remove this line!)
         first_msg = self.receiver()
+        
+        # initializing variables for time/depth limit for minimax
+        start_time = time.time()
+        max_depth = 3
 
         while True:
             msg = self.receiver()
@@ -93,6 +98,17 @@ class PlayerControllerMinimax(PlayerController):
         # NOTE: Don't forget to initialize the children of the current node
         #       with its compute_and_get_children() method!
 
+
+        # start time to keep track of time limit
+        startTime = time.time()
+        depth = 0
+        bestMoveInt = 0 # 0 is stay, 1 is left, 2 is right, 3 is up, 4 is down
+
+        # dictionary to keep track of visited nodes
+        visitedNodes = dict()
+
+
+        
         
         
 
@@ -103,11 +119,87 @@ class PlayerControllerMinimax(PlayerController):
 
 
 
-# distance calculator, euclidian, takes 2 tuples and returns distance between them
-def EuclidianDist(coord1: Tuple, coord2:Tuple)->float:
-    """takes in two coordinates in form of tuples and returns distance between them"""
+    def DepthSearch(rootNode:Node, depth:int, maxDepth:int, initialTime:int, maxTime:int, visitedNodes) -> Node:
+        """depth first search algorithm, using alpha beta pruning"""
 
-    return np.sqrt( (coord2(1) -coord1(1))**2 + (coord2(2) -coord1(2))**2  )
+        # check if the state is terminal
+        if CheckIfTerminalState(rootNode.state, depth, maxDepth, time.time() - initialTime, maxTime):
+            return rootNode
+        
+
+        # initial values for alpha and beta
+        alpha = -np.inf
+        beta = np.inf
+
+
+
+        # compute children of the node
+        children = rootNode.compute_and_get_children()
+
+        # each child is to be assigned a heuristic value, they are saved in a list, so can choose the best one
+        heuristicValuesChildren = []
+
+        # loop through children
+        for child in children:
+                
+            # check if child has been visited
+            if child in visitedNodes:
+                continue
+            
+            # add child to visited nodes
+            visitedNodes[child] = True
+
+              
+                
+
+        return Node()
+        
+
+    def AlphaBetaPruning(rootNode:Node, depth:int, maxDepth:int, initialTime:int, maxTime:int, alpha:int, beta:int) -> int:
+        """alpha beta pruning algorithm for minimax; 
+            returns the heuristic value of the node
+        """
+
+        # check if the state is terminal, raise exception
+        if CheckIfTerminalState(rootNode.state, depth, maxDepth, time.time() - initialTime, maxTime):
+            return RuntimeError("Error: reached time limit")
+        
+
+
+
+        
+        return 0
+
+
+
+    def HeuristicFunction(state:State, indxClosestFish) -> int:
+        """heuristic function for the minimax algorithm"""
+        
+        value = state.player_scores[0] - state.player_scores[1]
+
+
+        return 0
+
+
+
+
+
+
+# distance calculator, euclidian, takes 2 tuples and returns distance between them
+def ManhattanDistance(coord1: Tuple, coord2:Tuple)->float:
+    """takes in two coordinates in form of tuples and returns the manhattan distance between them
+        is |dx|+|dy|
+    """
+
+    # return np.sqrt( (coord2(1) -coord1(1))**2 + (coord2(2) -coord1(2))**2  )
+
+    # when being on the edge of the map one can make a step that gets you to the other side of the map
+    dx = np.min([np.abs(coord1[0] - coord2[0]), 20 - np.abs(coord1[0] - coord2[0])])
+    
+    # for y it is normal distance calculation
+    dy = np.abs(coord1[1] - coord2[1])
+
+    return dx + dy
 
 
 class ParentNode:
