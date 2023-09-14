3df6edf464dbf70a79a85f02937de2e51035d692
ashenawa <ashenawa@kth.se>
2023-09-14 15:15:54 +0200

    improved heuristic function

--- Search/player.py
+++ Search/player.py
@@ -8,6 +8,9 @@ from typing import List, Tuple # for type hinting
 import numpy as np
 import time
 
+
+
+
 class PlayerControllerHuman(PlayerController):
     def player_loop(self):
         """
@@ -29,6 +32,11 @@ class PlayerControllerHuman(PlayerController):
 class PlayerControllerMinimax(PlayerController):
 
     def __init__(self):
+        
+        # initializing variables for time/depth limit for minimax
+        self.timeLimit = 0.075 # time limit for each move in seconds, 75 ms
+        self.max_depth = 3
+        
         super(PlayerControllerMinimax, self).__init__()
         
 
@@ -37,13 +45,14 @@ class PlayerControllerMinimax(PlayerController):
         Main loop for the minimax next move search.
         :return:
         """
+        start_time = time.time()
 
         # Generate first message (Do not remove this line!)
         first_msg = self.receiver()
         
-        # initializing variables for time/depth limit for minimax
-        start_time = time.time()
-        max_depth = 3
+        
+
+        self.statesHashTable = dict()
 
         while True:
             msg = self.receiver()
@@ -119,7 +128,7 @@ class PlayerControllerMinimax(PlayerController):
 
 
 
-    def DepthSearch(rootNode:Node, depth:int, maxDepth:int, initialTime:int, maxTime:int, visitedNodes) -> Node:
+    def DepthSearch(self, rootNode:Node, depth:int, maxDepth:int, initialTime:int, maxTime:int, visitedNodes) -> Node:
         """depth first search algorithm, using alpha beta pruning"""
 
         # check if the state is terminal
@@ -155,7 +164,7 @@ class PlayerControllerMinimax(PlayerController):
         return Node()
         
 
-    def AlphaBetaPruning(rootNode:Node, depth:int, maxDepth:int, initialTime:int, maxTime:int, alpha:int, beta:int) -> int:
+    def AlphaBetaPruning(self, rootNode:Node, depth:int, maxDepth:int, initialTime:int, maxTime:int, alpha:int, beta:int) -> int:
         """alpha beta pruning algorithm for minimax; 
             returns the heuristic value of the node
         """
@@ -164,21 +173,104 @@ class PlayerControllerMinimax(PlayerController):
         if CheckIfTerminalState(rootNode.state, depth, maxDepth, time.time() - initialTime, maxTime):
             return RuntimeError("Error: reached time limit")
         
+        # check if state is in the hash table
+        hashedState = self.HashState(rootNode.state)
 
-
-
+        if hashedState in self.statesHashTable:
+            return self.statesHashTable[hashedState]
         
-        return 0
+        else:
 
 
 
-    def HeuristicFunction(state:State, indxClosestFish) -> int:
-        """heuristic function for the minimax algorithm"""
-        
-        value = state.player_scores[0] - state.player_scores[1]
-
+        # if depth == maxDepth or 
 
         return 0
+    
+    def HashState(self,state:State, depth:int) -> list:
+        """hashes the state to a list of integers"""
+        """
+        fishes don't move up/down - they stay on their y coordinate
+        we sum the x coordinates by multiplying each fish with their value
+        """
+
+        # list to store the hashed state
+        hashedState = []
+        firstTime = True
+        for fish in state.fishes_positions:
+            if firstTime: 
+                yCoord = fish[1]
+                firstTime = False
+                rowSum = 0
+            else:
+                if fish[1] == yCoord:
+                    rowSum += fish[0]*state.fishes_scores[fish]
+                else:
+                    hashedState.append(rowSum)
+                    yCoord = fish[1]
+                    rowSum = 0
+                    rowSum += fish[0]*state.fishes_scores[fish] 
+
+
+            # fish is a tuple with x and y coordinates
+            hashedState.append(rowSum)
+        hashedState.append(depth)
+        return hashedState
+
+
+
+    # def HeuristicFunction(self, state:State, indxClosestFish) -> int:
+    #     """heuristic function for the minimax algorithm"""
+    #     # todo: improve heuristic function
+
+    #     value = (state.player_scores[0] - state.player_scores[1])*10
+
+    #     return value
+
+    def HeuristicFunction(self, state: State) -> int:
+        """heuristic function for the minimax algorithm. Heuristic based on the score of the player, and the distance to the closest fish"""
+
+        # player scores 
+        playerScore = state.player_scores # player0 : playerScore[0], player1 : playerScore[1]
+
+        # check if any fish is caught
+        p0Caught, p1Caught = state.get_caught()
+
+        # check if player 0 caught a fish, if so add the value of the fish to the score, even if it is a negative value
+        # negative value => bad for player 0, good for player 1 => penalty for current state
+        if p0Caught != None:
+            fishCaught0 = state.fishes_scores[p0Caught]
+            closestFishScore = 0
+        else:
+            fishCaught0 = 0
+
+            #----- find closest fish -------
+            # all fish positions
+            fishPos = state.fish_positions.values() # list of tuples
+
+            # for each fish, calculate the distance to the player, and save the minimum distance
+            minDist = np.inf
+            for fish in fishPos:
+                dist = self.ManhattanDistance(state.get_hook_positions()[0], fish)
+                
+                # check that dist is less than minDist, and that fish does not have negative score
+                currFishScore = state.fishes_scores[fish]
+                if dist < minDist and currFishScore > 0:
+                    minDist = dist
+                    
+                    # closestFish = fish
+                    closestFishScore = currFishScore /10 # todo: double check this, thought: don't want being close to a fish to be too important, want to prioritize actually catching fish
+        
+        totalScore = playerScore[0] - playerScore[1] + fishCaught0 + closestFishScore
+        return totalScore
+            
+                
+
+                
+                
+
+            
+        
 
 
 
@@ -186,7 +278,7 @@ class PlayerControllerMinimax(PlayerController):
 
 
 # distance calculator, euclidian, takes 2 tuples and returns distance between them
-def ManhattanDistance(coord1: Tuple, coord2:Tuple)->float:
+def ManhattanDistance(self, coord1: Tuple, coord2:Tuple)->float:
     """takes in two coordinates in form of tuples and returns the manhattan distance between them
         is |dx|+|dy|
     """
