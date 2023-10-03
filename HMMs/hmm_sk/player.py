8240396de2cd35a7490ee685e5ceaff05bc0d88d
Tawsiful Islam <tawsiful11@hotmail.com>
2023-10-03 17:25:45 +0200

    Finished implementing code. Needs testing

--- HMMs/hmm_sk/player.py
+++ HMMs/hmm_sk/player.py
@@ -4,9 +4,8 @@ from player_controller_hmm import PlayerControllerHMMAbstract
 from constants import *
 import random
 from hmm3 import BaumWelch_Algo
+from hmm1 import ForwardAlgorithm, VectorMultiplication
 
-def init_pi(no_species: int):
-    pass
 
 def row_stoch(list_len: int, biased_mean = 0):
     if biased_mean == 0: 
@@ -17,7 +16,7 @@ def row_stoch(list_len: int, biased_mean = 0):
     row = []
     row_sum = 0
     for elem in range(list_len):
-        value = random.gauss(mean,std)
+        value = random.gauss(mean,std) #todo make sure to avoid negatives
         row.append(value)
         row_sum += value
     for elem in row:
@@ -49,9 +48,9 @@ class PlayerControllerHMM(PlayerControllerHMMAbstract):
         In this function you should initialize the parameters you will need,
         such as the initialization of models, or fishes, among others.
         """
-        self.fishModels = [HMM_model() for specie in range(N_SPECIES)]
-        self.fishObs = [(fish_id,[]) for fish_id in range(N_FISH)]
-        self.curr_fish_obs = []
+        self.fishModels = [HMM_model() for specie in range(N_SPECIES)]  #Initialise a model for each type of fish
+        self.fishObs = [(fish_id,[]) for fish_id in range(N_FISH)]  #Create a tuple for each individual fish which has a list that will have all observations
+        self.curr_fish_id = -1
 
     def guess(self, step, observations):
         """
@@ -66,7 +65,29 @@ class PlayerControllerHMM(PlayerControllerHMMAbstract):
         # This code would make a random guess on each step:
         # return (step % N_FISH, random.randint(0, N_SPECIES - 1))
 
-        return None
+        for fish_id in range(N_FISH):
+            self.fishObs[fish_id][1].append(observations[fish_id])    #Add observations for all fish
+
+        if step < 10: # Consider waiting for more observation
+            return None #Make no guesses until we have acquired enough observations
+        
+        self.curr_fish_id += 1
+        guessFishObs = self.fishObs[self.curr_fish_id][1]   #Extract observations for the individual fish we will guess
+        bestModelProb = 0
+        bestFishType = 0
+        for fishType in range(N_SPECIES): #For each fish specie
+            """Get the fish and it's observation
+            For each specie, calculate which model has the highest probability for this particular fish with its observations
+            """
+            fishTypeModel = self.fishModels[fishType]
+            alpha0 = VectorMultiplication(fishTypeModel.pi,fishTypeModel.B[guessFishObs[0]])    #Dot-product using initial probability (pi) with emission probability for fishType (B[fishType]) at t=0
+            fishTypeProb = ForwardAlgorithm(alpha0,fishTypeModel.B,guessFishObs,fishTypeModel.A,len(fishTypeModel.A[0]),len(fishTypeModel.A))
+            if fishTypeProb > bestModelProb:    #Saving which guess gave best probability
+                bestModelProb = fishTypeProb
+                bestFishType = fishType
+
+        return (self.curr_fish_id, bestFishType)
+        
 
     def reveal(self, correct, fish_id, true_type):
         """
@@ -78,4 +99,5 @@ class PlayerControllerHMM(PlayerControllerHMMAbstract):
         :param true_type: the correct type of the fish
         :return:
         """
-        pass
+        if not correct:
+           self.fishModels[true_type].updateModel(self.fishObs[fish_id])    # After knowing correct type, take its previous model and update it with Baum-Welch together with this particular fish_id's observation
